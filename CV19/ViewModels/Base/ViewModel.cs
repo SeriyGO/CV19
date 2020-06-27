514beb803473b12ecf4a34561f1d385ad7ed8875
Sergey Tsurkan <serezatsurka@gmail.com>
2020-06-27 13:18:06 +0300

    Added Idisposible to ViewModel

--- CV19/ViewModels/Base/ViewModel.cs
+++ CV19/ViewModels/Base/ViewModel.cs
@@ -1,11 +1,13 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
 namespace CV19.ViewModels.Base
 {
-  internal abstract class ViewModel:INotifyPropertyChanged
+  internal abstract class ViewModel:INotifyPropertyChanged,IDisposable
   {
     public event PropertyChangedEventHandler PropertyChanged;
+
     protected virtual void OnPropertyChanged(string PropertyName)
     {
       PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
@@ -17,5 +19,17 @@ namespace CV19.ViewModels.Base
       OnPropertyChanged(PropertyName);
       return true;
     }
+    public void Dispose()
+    {
+      Dispose(true);
+      //Освобождение управляемых ресурсов
+    }
+    public void Dispose(bool Disposing)
+    {
+      if (!Disposing || _Disposed)
+        _Disposed = true;
+      //Освобождение управляемых ресурсов
+    }
+    private bool _Disposed;
   }
 }
