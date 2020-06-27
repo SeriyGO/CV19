f88cb1b720a35e97fde9bea12ebe6641915f7c3a
Sergey Tsurkan <serezatsurka@gmail.com>
2020-06-27 23:07:51 +0300

    Some was changed

--- CV19/ViewModels/MainWindowViewModel.cs
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -1,10 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using CV19.ViewModels.Base;
 
 namespace CV19.ViewModels
 {
-  class MainWindowViewModel
+  internal class MainWindowViewModel:ViewModel
   {
+    #region Заголовок окна
+    private string _Title="Analiz staticstic of CV19";
+    public string Title 
+    {
+      get => _Title;
+      set => Set(ref _Title, value);
+    }
+    #endregion
+
   }
 }
